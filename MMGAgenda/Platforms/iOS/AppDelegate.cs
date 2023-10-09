62cf0fe0718c65d1a4224b81ea3a966126c05a2d
EdoardoGiussani <edoardo.giussani@outlook.com>
2023-10-09 17:18:36 +0200

    Add project files.

---
+++ MMGAgenda/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MMGAgenda
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
